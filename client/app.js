c54a90d73627dc79bb9ecd4568d2f55b3aeb011f
Pranshu0808 <pranshusharma789123@gmail.com>
2026-08-06 03:13:20 +0530

    Update app.js

--- client/app.js
+++ client/app.js
@@ -35,7 +35,7 @@ socket.onmessage = (event) => {
 
         if (!sensorObj) return;
 
-       const pressure = Number(((sensor.sensorValue * 50) / 4095).toFixed(1));
+       const pressure = Number(((sensor.sensorValue * 4095) / 4095).toFixed(1));
 
       sensorObj.value = pressure;
       sensorObj.active = true;
